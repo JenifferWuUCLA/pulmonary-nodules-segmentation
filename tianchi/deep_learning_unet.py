29f0fe399f4e593c2b96e6380611e639b6990b1e
root <yywu@szucla.org>
2017-07-05 09:50:47 +0800

    Rollback image mask size to 512*512

--- tianchi/deep_learning_unet.py
+++ tianchi/deep_learning_unet.py
@@ -12,15 +12,15 @@ import cv2
 
 
 # subset = "train_dataset/"
-subset = "nerve-tianchi/"
+subset = "nerve-mine/"
 # working_path = "/home/ucla/Downloads/tianchi/" + subset
 output_path = "/home/jenifferwu/IMAGE_MASKS_DATA/" + subset
 
 
 K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
 
-img_rows = 256
-img_cols = 256
+img_rows = 512
+img_cols = 512
 
 smooth = 1.
 
@@ -142,7 +142,7 @@ def train_and_predict(use_existing):
     print('-' * 30)
     num_test = len(imgs_test)
     print("num_test: %d" % num_test)
-    imgs_mask_test = np.ndarray([num_test, 1, 256, 256], dtype=np.float32)
+    imgs_mask_test = np.ndarray([num_test, 1, 512, 512], dtype=np.float32)
     for i in range(num_test):
         imgs_mask_test[i] = model.predict([imgs_test[i:i + 1]], verbose=0)[0]
     np.save(os.path.join(output_path + "preds/", 'masksTestPredicted.npy'), imgs_mask_test)

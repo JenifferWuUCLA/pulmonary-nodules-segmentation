60497286e922a987d5a635cf8f077cf10287fe7f
root <yywu@szucla.org>
2017-07-28 15:00:20 +0800

    Add nodule classification using caffe CNN.

---
+++ caffe-recognition/nodule_classification.py
@@ -0,0 +1,111 @@
+# 1. Setup
+# set up Python environment: numpy for numerical routines, and matplotlib for plotting
+import numpy as np
+from glob import glob
+
+# The caffe module needs to be on the Python path;
+#  we'll add it here explicitly.
+import sys
+caffe_root = '/root/code/caffe/'  # this file should be run from {caffe_root}/examples (otherwise change this line)
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+# If you get "No module named _caffe", either you have not built pycaffe or you have the wrong path.
+
+import os
+if os.path.isfile(caffe_root + 'models/pulmonary_nodules_net_caffenet/caffenet_train_iter_30000.caffemodel'):
+    print('CaffeNet found.')
+else:
+    print('CaffeNet not found.')
+
+# 2. Load net and set up input preprocessing
+caffe.set_mode_cpu()
+
+model_def = caffe_root + 'models/pulmonary_nodules_net_caffenet/deploy.prototxt'
+model_weights = caffe_root + 'models/pulmonary_nodules_net_caffenet/caffenet_train_iter_30000.caffemodel'
+
+net = caffe.Net(model_def,      # defines the structure of the model
+                model_weights,  # contains the trained weights
+                caffe.TEST)     # use test mode (e.g., don't perform dropout)
+
+# load the mean PulmonaryNodules image (as distributed with Caffe) for subtraction
+mu = np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy')
+mu = mu.mean(1).mean(1)  # average over pixels to obtain the mean (BGR) pixel values
+print('mean-subtracted values:', zip('BGR', mu))
+
+# create transformer for the input called 'data'
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+
+transformer.set_transpose('data', (2,0,1))  # move image channels to outermost dimension
+transformer.set_mean('data', mu)            # subtract the dataset-mean value in each channel
+transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
+transformer.set_channel_swap('data', (2,1,0))  # swap channels from RGB to BGR
+
+# 3. CPU classification
+subset = "val_subset_all/"
+output_path = "/home/ucla/Downloads/tianchi/" + subset
+# set the size of the input (we can skip this if we're happy
+#  with the default; we can also change it later, e.g., for different batch sizes)
+net.blobs['data'].reshape(50,        # batch size
+                          3,         # 3-channel (BGR) images
+                          227, 227)  # image size is 227x227
+
+test_images = glob(os.path.join(output_path, "images_*.jpg"))
+for test_image in test_images:
+    image = caffe.io.load_image(caffe_root + test_image)
+    transformed_image = transformer.preprocess('data', image)
+
+    # copy the image data into the memory allocated for the net
+    net.blobs['data'].data[...] = transformed_image
+
+    # Switching to GPU mode
+    caffe.set_device(0)  # if we have multiple GPUs, pick the first one
+    caffe.set_mode_gpu()
+    ### perform classification
+    output = net.forward()
+
+    output_prob = output['prob'][0]  # the output probability vector for the first image in the batch
+
+    print('predicted class is:', output_prob.argmax())
+
+    # load PulmonaryNodulesNet labels
+    labels_file = caffe_root + 'data/pulmonary_nodules/synset_words.txt'
+    # if not os.path.exists(labels_file):
+    #     !../data/pulmonary_nodules/get_pulmonary_nodules.sh
+
+    labels = np.loadtxt(labels_file, str, delimiter='\t')
+
+    print('output label:', labels[output_prob.argmax()])
+
+    # sort top five predictions from softmax output
+    top_inds = output_prob.argsort()[::-1][:5]  # reverse sort and take five largest items
+
+    print('probabilities and labels:')
+    zip(output_prob[top_inds], labels[top_inds])
+
+# 4. Switching to GPU mode
+
+# 6. Try your own image
+subset = "val_subset_all/"
+output_path = "/home/ucla/Downloads/tianchi/" + subset
+# transform it and copy it into the net
+image = caffe.io.load_image(output_path + 'image.jpg')
+net.blobs['data'].data[...] = transformer.preprocess('data', image)
+
+# Switching to GPU mode
+caffe.set_device(0)  # if we have multiple GPUs, pick the first one
+caffe.set_mode_gpu()
+# perform classification
+net.forward()
+
+# obtain the output probabilities
+output_prob = net.blobs['prob'].data[0]
+
+labels = np.loadtxt(labels_file, str, delimiter='\t')
+print('output label:', labels[output_prob.argmax()])
+
+# sort top five predictions from softmax output
+top_inds = output_prob.argsort()[::-1][:5]
+
+print('probabilities and labels:')
+zip(output_prob[top_inds], labels[top_inds])

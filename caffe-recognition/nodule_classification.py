c68726a0b78b03cbb5c4e2f064a7f63bdf61fb7e
root <yywu@szucla.org>
2017-07-28 15:27:40 +0800

    Resolve the bugs.

--- caffe-recognition/nodule_classification.py
+++ caffe-recognition/nodule_classification.py
@@ -1,18 +1,27 @@
 # 1. Setup
 # set up Python environment: numpy for numerical routines, and matplotlib for plotting
 import numpy as np
+import sys
+import caffe
+import os
 from glob import glob
+
+csvRows = []
+
+def csv_row(image_name, probability, label):
+    new_row = []
+    new_row.append(image_name)
+    new_row.append(probability)
+    new_row.append(label)
+    csvRows.append(new_row)
+
 
 # The caffe module needs to be on the Python path;
 #  we'll add it here explicitly.
-import sys
 caffe_root = '/root/code/caffe/'  # this file should be run from {caffe_root}/examples (otherwise change this line)
 sys.path.insert(0, caffe_root + 'python')
 
-import caffe
 # If you get "No module named _caffe", either you have not built pycaffe or you have the wrong path.
-
-import os
 if os.path.isfile(caffe_root + 'models/pulmonary_nodules_net_caffenet/caffenet_train_iter_30000.caffemodel'):
     print('CaffeNet found.')
 else:
@@ -24,9 +33,9 @@ caffe.set_mode_cpu()
 model_def = caffe_root + 'models/pulmonary_nodules_net_caffenet/deploy.prototxt'
 model_weights = caffe_root + 'models/pulmonary_nodules_net_caffenet/caffenet_train_iter_30000.caffemodel'
 
-net = caffe.Net(model_def,      # defines the structure of the model
+net = caffe.Net(model_def,  # defines the structure of the model
                 model_weights,  # contains the trained weights
-                caffe.TEST)     # use test mode (e.g., don't perform dropout)
+                caffe.TEST)  # use test mode (e.g., don't perform dropout)
 
 # load the mean PulmonaryNodules image (as distributed with Caffe) for subtraction
 mu = np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy')
@@ -36,23 +45,23 @@ print('mean-subtracted values:', zip('BGR', mu))
 # create transformer for the input called 'data'
 transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
 
-transformer.set_transpose('data', (2,0,1))  # move image channels to outermost dimension
-transformer.set_mean('data', mu)            # subtract the dataset-mean value in each channel
-transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
-transformer.set_channel_swap('data', (2,1,0))  # swap channels from RGB to BGR
+transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost dimension
+transformer.set_mean('data', mu)  # subtract the dataset-mean value in each channel
+transformer.set_raw_scale('data', 255)  # rescale from [0, 1] to [0, 255]
+transformer.set_channel_swap('data', (2, 1, 0))  # swap channels from RGB to BGR
 
 # 3. CPU classification
 subset = "val_subset_all/"
 output_path = "/home/ucla/Downloads/tianchi/" + subset
 # set the size of the input (we can skip this if we're happy
 #  with the default; we can also change it later, e.g., for different batch sizes)
-net.blobs['data'].reshape(50,        # batch size
-                          3,         # 3-channel (BGR) images
+net.blobs['data'].reshape(50,  # batch size
+                          3,  # 3-channel (BGR) images
                           227, 227)  # image size is 227x227
 
 test_images = glob(os.path.join(output_path, "images_*.jpg"))
 for test_image in test_images:
-    image = caffe.io.load_image(caffe_root + test_image)
+    image = caffe.io.load_image(test_image)
     transformed_image = transformer.preprocess('data', image)
 
     # copy the image data into the memory allocated for the net
@@ -81,14 +90,20 @@ for test_image in test_images:
     top_inds = output_prob.argsort()[::-1][:5]  # reverse sort and take five largest items
 
     print('probabilities and labels:')
-    zip(output_prob[top_inds], labels[top_inds])
+    z_list = zip(output_prob[top_inds], labels[top_inds])
+    print(z_list)
+    for index in range(len(z_list)):
+        z_item = z_list[index]
+        probability, label = z_item[0], z_item[1]
+        csv_row(test_image, probability, label)
 
 
 # 6. Try your own image
 subset = "val_subset_all/"
 output_path = "/home/ucla/Downloads/tianchi/" + subset
 # transform it and copy it into the net
-image = caffe.io.load_image(output_path + 'image.jpg')
+test_image = "images_0000_0000_0.jpg "
+image = caffe.io.load_image(output_path + test_image)
 net.blobs['data'].data[...] = transformer.preprocess('data', image)
 
 # Switching to GPU mode
@@ -107,4 +122,9 @@ print('output label:', labels[output_prob.argmax()])
 top_inds = output_prob.argsort()[::-1][:5]
 
 print('probabilities and labels:')
-zip(output_prob[top_inds], labels[top_inds])
+z_list = zip(output_prob[top_inds], labels[top_inds])
+print(z_list)
+for index in range(len(z_list)):
+    z_item = z_list[index]
+    probability, label = z_item[0], z_item[1]
+    csv_row(test_image, probability, label)

1676da8f652a620a8104940d1231b66d94416923
root <yywu@szucla.org>
2017-08-02 11:12:05 +0800

    Modify to add Caffe_Root.

--- caffe-recognition/nodule_convert_protomean.py
+++ caffe-recognition/nodule_convert_protomean.py
@@ -1,9 +1,12 @@
 import caffe
 import numpy as np
 
+
+caffe_root = '/root/code/caffe/'
+
 blob = caffe.proto.caffe_pb2.BlobProto()
-data = open('data/pulmonary_nodules/pulmonary_nodules_net_mean.binaryproto', 'rb').read()
+data = open(caffe_root + 'data/pulmonary_nodules/pulmonary_nodules_net_mean.binaryproto', 'rb').read()
 blob.ParseFromString(data)
 arr = np.array(caffe.io.blobproto_to_array(blob))
 out = arr[0]
-np.save('python/caffe/pulmonary_nodules/pulmonary_nodules_net_mean.npy', out)
+np.save(caffe_root + 'python/caffe/pulmonary_nodules_net/pulmonary_nodules_net_mean.npy', out)

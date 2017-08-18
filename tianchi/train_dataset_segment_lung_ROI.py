6df94f68147d3128a7f5b76d35968a2478887297
root <yywu@szucla.org>
2017-08-18 15:47:22 +0800

    Preprocessing of train set and val set.

--- tianchi/train_dataset_segment_lung_ROI.py
+++ tianchi/train_dataset_segment_lung_ROI.py
@@ -4,15 +4,19 @@ import numpy as np
 from skimage import morphology
 from skimage import measure
 from sklearn.cluster import KMeans
-from skimage.transform import resize
 from glob import glob
 import os
+import cv2
+import scipy.ndimage
 
 
 out_subset = "nerve-mine-2D"
-# out_subset = "server-test-2D"
+# out_subset = "z-nerve"
 output_path = "/home/ucla/Downloads/tianchi-2D/" + out_subset
 # output_path = "/home/jenifferwu/IMAGE_MASKS_DATA/" + out_subset
+
+tmp_workspace = os.path.join(output_path, "train/")
+tmp_jpg_workspace = os.path.join(output_path, "ROI/train/")
 
 
 train_images = glob(os.path.join(output_path, "train/images_*.npy"))
@@ -46,15 +50,15 @@ for img_file in train_images:
         kmeans = KMeans(n_clusters=2).fit(np.reshape(middle,[np.prod(middle.shape),1]))
         centers = sorted(kmeans.cluster_centers_.flatten())
         threshold = np.mean(centers)
-        thresh_img = np.where(img<threshold,1.0,0.0)  # threshold the image
+        thresh_img = np.where(img<threshold, 1.0, 0.0)  # threshold the image
         #
         # I found an initial erosion helful for removing graininess from some of the regions
         # and then large dialation is used to make the lung region 
         # engulf the vessels and incursions into the lung cavity by 
         # radio opaque tissue
         #
-        eroded = morphology.erosion(thresh_img,np.ones([4,4]))
-        dilation = morphology.dilation(eroded,np.ones([10,10]))
+        eroded = morphology.erosion(thresh_img, np.ones([4, 4]))
+        dilation = morphology.dilation(eroded, np.ones([10, 10]))
         #
         #  Label each region and obtain the region properties
         #  The background region is removed by removing regions 
@@ -72,9 +76,9 @@ for img_file in train_images:
         good_labels = []
         for prop in regions:
             B = prop.bbox
-            if B[2]-B[0]<475 and B[3]-B[1]<475 and B[0]>40 and B[2]<472:
+            if B[2] - B[0] < 475 and B[3] - B[1] < 475 and B[0] > 40 and B[2] < 472:
                 good_labels.append(prop.label)
-        mask = np.ndarray([512,512],dtype=np.int8)
+        mask = np.ndarray([512, 512], dtype=np.int8)
         mask[:] = 0
         #
         #  The mask here is the mask for the lungs--not the nodes
@@ -82,10 +86,10 @@ for img_file in train_images:
         #  in order to fill in and out the lung mask 
         #
         for N in good_labels:
-            mask = mask + np.where(labels==N,1,0)
-        mask = morphology.dilation(mask,np.ones([10,10])) # one last dilation
+            mask = mask + np.where(labels == N, 1, 0)
+        mask = morphology.dilation(mask, np.ones([10, 10]))  # one last dilation
         imgs_to_process[i] = mask
-    np.save(img_file.replace("images","lungmask"),imgs_to_process)
+    np.save(img_file.replace("images", "lungmask"), imgs_to_process)
     
 
 #
@@ -98,85 +102,52 @@ out_images = []      #final set of images
 out_nodemasks = []   #final set of nodemasks
 for fname in train_images:
     print("working on train lung mask file: %s" % fname)
-    imgs_to_process = np.load(fname.replace("lungmask","images"))
+    imgs_to_process = np.load(fname.replace("lungmask", "images"))
     masks = np.load(fname)
-    node_masks = np.load(fname.replace("lungmask","masks"))
+    node_masks = np.load(fname.replace("lungmask", "masks"))
     for i in range(len(imgs_to_process)):
         mask = masks[i]
         node_mask = node_masks[i]
         img = imgs_to_process[i]
-        new_size = [512,512]   # we're scaling back up to the original size of the image
-        img= mask*img          # apply lung mask
-        #
-        # renormalizing the masked image (in the mask region)
-        #
-        new_mean = np.mean(img[mask>0])  
-        new_std = np.std(img[mask>0])
-        #
-        #  Pulling the background color up to the lower end
-        #  of the pixel range for the lungs
-        #
-        old_min = np.min(img)       # background color
-        img[img==old_min] = new_mean-1.2*new_std   # resetting backgound color
-        img = img-new_mean
-        img = img/new_std
-        #make image bounding box  (min row, min col, max row, max col)
-        labels = measure.label(mask)
-        regions = measure.regionprops(labels)
-        #
-        # Finding the global min and max row over all regions
-        #
-        min_row = 512
-        max_row = 0
-        min_col = 512
-        max_col = 0
-        for prop in regions:
-            B = prop.bbox
-            if min_row > B[0]:
-                min_row = B[0]
-            if min_col > B[1]:
-                min_col = B[1]
-            if max_row < B[2]:
-                max_row = B[2]
-            if max_col < B[3]:
-                max_col = B[3]
-        width = max_col-min_col
-        height = max_row - min_row
-        if width > height:
-            max_row=min_row+width
-        else:
-            max_col = min_col+height
-        # 
-        # cropping the image down to the bounding box for all regions
-        # (there's probably an skimage command that can do this in one line)
-        # 
-        img = img[min_row:max_row,min_col:max_col]
-        mask =  mask[min_row:max_row,min_col:max_col]
-        if max_row-min_row <5 or max_col-min_col<5:  # skipping all images with no god regions
-            pass
-        else:
-            # moving range to -1 to 1 to accomodate the resize function
-            mean = np.mean(img)
-            img = img - mean
-            min = np.min(img)
-            max = np.max(img)
-            img = img/(max-min)
-            new_img = resize(img,[512,512])
-            new_node_mask = resize(node_mask[min_row:max_row,min_col:max_col],[512,512])
-            out_images.append(new_img)
-            out_nodemasks.append(new_node_mask)
+        # new_size = [512, 512]   # we're scaling back up to the original size of the image
+        img = mask * img          # apply lung mask
+
+        slice = img * mask
+        filename = fname.replace(tmp_workspace, "").replace("lungmask", "lung_images")
+        new_lung_name = filename.replace(".npy", "") + "_%s.jpg" % (i)
+        image_path = tmp_jpg_workspace
+        print(new_lung_name, image_path)
+        cv2.imwrite(os.path.join(image_path, new_lung_name), slice)
+
+        node_mask = scipy.ndimage.interpolation.zoom(node_mask, [1.0, 1.0], mode='nearest')
+        node_mask[node_mask < 0.5] = 0
+        node_mask[node_mask > 0.5] = 1
+        node_mask = node_mask.astype('int8')
+        node_mask = 255.0 * node_mask
+        node_mask = node_mask.astype(np.uint8)
+
+        filename = fname.replace(tmp_workspace, "").replace("lungmask", "node_mask")
+        new_node_name = filename.replace(".npy", "") + "_%s.jpg" % (i)
+        image_path = tmp_jpg_workspace
+        print(new_node_name, image_path)
+        cv2.imwrite(os.path.join(image_path, new_node_name), node_mask)
+
+        new_img = slice
+        new_node_mask = node_mask
+        out_images.append(new_img)
+        out_nodemasks.append(new_node_mask)
 
 num_images = len(out_images)
 #
 #  Writing out images and masks as 1 channel arrays for input into network
 #
-final_images = np.ndarray([num_images,1,512,512],dtype=np.float32)
-final_masks = np.ndarray([num_images,1,512,512],dtype=np.float32)
+final_images = np.ndarray([num_images, 1, 512, 512], dtype=np.float32)
+final_masks = np.ndarray([num_images, 1, 512, 512], dtype=np.float32)
 for i in range(num_images):
     final_images[i,0] = out_images[i]
     final_masks[i,0] = out_nodemasks[i]
 
-rand_i = np.random.choice(range(num_images),size=num_images,replace=False)
+rand_i = np.random.choice(range(num_images), size=num_images, replace=False)
 # test_i = int(0.2*num_images)
 
 np.save(os.path.join(output_path, "train/trainImages.npy"), final_images[rand_i[:]])
--- tianchi/val_dataset_segment_lung_ROI.py
+++ tianchi/val_dataset_segment_lung_ROI.py
@@ -4,16 +4,21 @@ import numpy as np
 from skimage import morphology
 from skimage import measure
 from sklearn.cluster import KMeans
-from skimage.transform import resize
 from glob import glob
 import os
 import csv
+import cv2
+import scipy.ndimage
 
 
 out_subset = "nerve-mine-2D"
 # out_subset = "z-nerve"
 output_path = "/home/ucla/Downloads/tianchi-2D/" + out_subset
 # output_path = "/home/jenifferwu/IMAGE_MASKS_DATA/" + out_subset
+
+tmp_workspace = os.path.join(output_path, "val/")
+tmp_jpg_workspace = os.path.join(output_path, "ROI/val/")
+
 
 ###################################################################################
 
@@ -120,69 +125,37 @@ for fname in val_images:
         mask = masks[i]
         node_mask = node_masks[i]
         img = imgs_to_process[i]
-        new_size = [512, 512]  # we're scaling back up to the original size of the image
+        # new_size = [512, 512]  # we're scaling back up to the original size of the image
         img = mask * img  # apply lung mask
-        #
-        # renormalizing the masked image (in the mask region)
-        #
-        new_mean = np.mean(img[mask > 0])
-        new_std = np.std(img[mask > 0])
-        #
-        #  Pulling the background color up to the lower end
-        #  of the pixel range for the lungs
-        #
-        old_min = np.min(img)  # background color
-        img[img == old_min] = new_mean - 1.2 * new_std  # resetting backgound color
-        img = img - new_mean
-        img = img / new_std
-        # make image bounding box  (min row, min col, max row, max col)
-        labels = measure.label(mask)
-        regions = measure.regionprops(labels)
-        #
-        # Finding the global min and max row over all regions
-        #
-        min_row = 512
-        max_row = 0
-        min_col = 512
-        max_col = 0
-        for prop in regions:
-            B = prop.bbox
-            if min_row > B[0]:
-                min_row = B[0]
-            if min_col > B[1]:
-                min_col = B[1]
-            if max_row < B[2]:
-                max_row = B[2]
-            if max_col < B[3]:
-                max_col = B[3]
-        width = max_col - min_col
-        height = max_row - min_row
-        if width > height:
-            max_row = min_row + width
-        else:
-            max_col = min_col + height
-        #
-        # cropping the image down to the bounding box for all regions
-        # (there's probably an skimage command that can do this in one line)
-        #
-        img = img[min_row:max_row, min_col:max_col]
-        mask = mask[min_row:max_row, min_col:max_col]
-        if max_row - min_row < 5 or max_col - min_col < 5:  # skipping all images with no god regions
-            pass
-        else:
-            # moving range to -1 to 1 to accomodate the resize function
-            mean = np.mean(img)
-            img = img - mean
-            min = np.min(img)
-            max = np.max(img)
-            img = img / (max - min)
-            new_img = resize(img, [512, 512])
-            new_node_mask = resize(node_mask[min_row:max_row, min_col:max_col], [512, 512])
-            out_images.append(new_img)
-            out_nodemasks.append(new_node_mask)
-            image_path = fname.replace("lungmask", "images")
-            image_name = image_path.replace(os.path.join(output_path, "val/"), "").replace("images_", "")
-            seriesuids.append(image_name.replace(".npy", ""))
+
+        slice = img * mask
+        filename = fname.replace(tmp_workspace, "").replace("lungmask", "lung_images")
+        new_lung_name = filename.replace(".npy", "") + "_%s.jpg" % (i)
+        image_path = tmp_jpg_workspace
+        print(new_lung_name, image_path)
+        cv2.imwrite(os.path.join(image_path, new_lung_name), slice)
+
+        node_mask = scipy.ndimage.interpolation.zoom(node_mask, [1.0, 1.0], mode='nearest')
+        node_mask[node_mask < 0.5] = 0
+        node_mask[node_mask > 0.5] = 1
+        node_mask = node_mask.astype('int8')
+        node_mask = 255.0 * node_mask
+        node_mask = node_mask.astype(np.uint8)
+
+        filename = fname.replace(tmp_workspace, "").replace("lungmask", "node_mask")
+        new_node_name = filename.replace(".npy", "") + "_%s.jpg" % (i)
+        image_path = tmp_jpg_workspace
+        print(new_node_name, image_path)
+        cv2.imwrite(os.path.join(image_path, new_node_name), node_mask)
+
+        new_img = slice
+        new_node_mask = node_mask
+        out_images.append(new_img)
+        out_nodemasks.append(new_node_mask)
+
+        image_path = fname.replace("lungmask", "images")
+        image_name = image_path.replace(os.path.join(output_path, "val/"), "").replace("images_", "") + "_%s.jpg" % (i)
+        seriesuids.append(image_name.replace(".npy", ""))
 
 num_images = len(out_images)
 #

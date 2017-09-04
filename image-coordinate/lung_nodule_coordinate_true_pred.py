830bdbaa3058d90c37dc73b35d90d8f22dd4b941
root <yywu@szucla.org>
2017-09-04 14:20:18 +0800

    Modify the bugs on lung nodule prediction error.

--- image-coordinate/lung_nodule_coordinate_true_pred.py
+++ image-coordinate/lung_nodule_coordinate_true_pred.py
@@ -1,6 +1,7 @@
 import csv, os
 
 csv_path = "/home/ucla/Downloads/tianchi-2D/csv"
+# csv_path = "/home/jenifferwu/IMAGE_MASKS_DATA/z-nerve/csv"
 annotations_true_file = os.path.join(csv_path, "annotations.csv")
 annotations_pred_file = os.path.join(csv_path, "imgs_mask_test_coordinate.csv")
 statistics_file = os.path.join(csv_path, "statistics.csv")
@@ -33,7 +34,7 @@ csvFileObj.close()
 
 # Read the imgs_mask_test_coordinate.csv in.
 pred_csvRows = []
-csvFileObj = open(annotations_true_file)
+csvFileObj = open(annotations_pred_file)
 readerObj = csv.reader(csvFileObj)
 for row in readerObj:
     pred_csvRows.append(row)
@@ -41,17 +42,25 @@ csvFileObj.close()
 
 csv_row("seriesuid", "coordX-error", "coordY-error", "coordZ-error", "diameter_mm-error")
 for true_row in true_csvRows:
-    true_seriesuid = true_row['seriesuid']
-    true_coordX = true_row["coordX"]
-    true_coordY = true_row["coordY"]
-    true_coordZ = true_row["coordZ"]
-    true_diameter_mm = true_row["diameter_mm"]
+    # print("true_row: ")
+    # print(true_row)
+    true_seriesuid = true_row[0]
+    true_coordX = true_row[1]
+    true_coordY = true_row[2]
+    true_coordZ = true_row[3]
+    true_diameter_mm = true_row[4]
+    # print("True value: ")
+    # print(true_seriesuid, true_coordX, true_coordY, true_coordZ, true_diameter_mm)
     for pred_row in pred_csvRows:
-        pred_seriesuid = pred_row['seriesuid']
-        pred_coordX = pred_row["coordX"]
-        pred_coordY = pred_row["coordY"]
-        pred_coordZ = pred_row["coordZ"]
-        pred_diameter_mm = pred_row["diameter_mm"]
+        # print("pred_row: ")
+        # print(pred_row)
+        pred_seriesuid = pred_row[0]
+        pred_coordX = pred_row[1]
+        pred_coordY = pred_row[2]
+        pred_coordZ = pred_row[3]
+        pred_diameter_mm = pred_row[4]
+        # print("Prediction value: ")
+        # print(pred_seriesuid, pred_coordX, pred_coordY, pred_coordZ, pred_diameter_mm)
         if true_seriesuid == pred_seriesuid:
             coordX_error = abs(float(true_coordX) - float(pred_coordX))
             coordY_error = abs(float(true_coordY) - float(pred_coordY))
--- image-coordinate/lung_nodule_predict_error_curve.py
+++ image-coordinate/lung_nodule_predict_error_curve.py
@@ -11,22 +11,38 @@ plt.rcParams['image.interpolation'] = 'nearest'  # don't interpolate: show squar
 plt.rcParams['image.cmap'] = 'gray'  # use grayscale output rather than a (potentially misleading) color heatmap
 
 csv_path = "/home/ucla/Downloads/tianchi-2D/csv"
+# csv_path = "/home/jenifferwu/IMAGE_MASKS_DATA/z-nerve/csv"
 statistics_file = os.path.join(csv_path, "statistics.csv")
 
 headers = ['seriesuid', 'coordX-error', 'coordY-error', 'coordZ-error', 'diameter_mm-error']
 df = pd.read_csv(statistics_file, names=headers)
 print (df)
 
-x = df['seriesuid']
-y = df['coordX-error']
-z = df['coordY-error']
-w = df['coordZ-error']
-u = df['diameter_mm-error']
+seriesuid = df['seriesuid']
+coordX_error = df['coordX-error'].astype(float)
+coordY_error = df['coordY-error'].astype(float)
+coordZ_error = df['coordZ-error'].astype(float)
+diameter_mm_error = df['diameter_mm-error'].astype(float)
+
+x = range(len(seriesuid))
+y_1 = coordX_error
+y_2 = coordY_error
+y_3 = coordZ_error
+d = diameter_mm_error
+# plt.plot(x, y, 'ro-')
+# plt.xticks(x, seriesuid, rotation=45)
+# plt.margins(0.08)
+# plt.subplots_adjust(bottom=0.15)
+# plt.show()
 
 # plot
-plt.plot(x, y, label="$coordX-error$", color="red", linewidth=2)
-plt.plot(x, z, label="coordY-error$", color="green", linewidth=2)
-plt.plot(x, w, label="coordZ-error$", color="blue", linewidth=2)
-plt.plot(x, u, "b--", label="diameter_mm-error$", color="yellow", linewidth=2)
+fig = plt.figure()
+ax = plt.subplot(111)
+ax.plot(x, y_1, 'ro-', label='$y = coordX-error', color="purple", linewidth=2)
+ax.plot(x, y_2, 'ro-', label='$y = coordY-error', color="green", linewidth=2)
+ax.plot(x, y_3, 'ro-', label='$y = coordZ-error', color="blue", linewidth=2)
+ax.plot(x, d, 'ro-', label='$y = diameter_mm-error', color="red", linewidth=2)
+plt.title('Legend lung nodule prediction error.')
+ax.legend()
 
 plt.show()

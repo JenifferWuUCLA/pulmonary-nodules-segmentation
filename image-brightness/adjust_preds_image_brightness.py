6586e2c4d25796920e5aada5e9dd69fbd4c950ec
root <yywu@szucla.org>
2017-09-19 16:28:04 +0800

    Resolve the bugs on the server.

--- image-brightness/adjust_preds_image_brightness.py
+++ image-brightness/adjust_preds_image_brightness.py
@@ -2,7 +2,7 @@ import cv2
 from glob import glob
 import os
 
-# out_subset = "server-test-3D/"
+# out_subset = "z-nerve/"
 output_path = "/home/ucla/Downloads/tianchi-2D/"
 # output_path = "/home/jenifferwu/IMAGE_MASKS_DATA/" + out_subset
 

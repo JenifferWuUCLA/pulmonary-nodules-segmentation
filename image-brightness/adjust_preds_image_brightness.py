a150ac06ba0b99d63c4ecc779fa00078011e5b74
root <yywu@szucla.org>
2017-10-09 10:52:50 +0800

    Flage image name before brightness.

--- image-brightness/adjust_preds_image_brightness.py
+++ image-brightness/adjust_preds_image_brightness.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
     for fn in test_images:
         print('loading %s ...' % fn)
         print('processing...')
+        image_name = fn.replace(output_path + "image-coordinate-2D/", "")
+        print("image_name: %s" % image_name)
         img = cv2.imread(fn)
         w = img.shape[1]
         h = img.shape[0]
@@ -26,7 +28,8 @@ if __name__ == '__main__':
                 img[xj, xi, 1] = int(img[xj, xi, 1] * 100)
                 img[xj, xi, 2] = int(img[xj, xi, 2] * 100)
         # cv2.imshow('img', img)
-        cv2.imwrite(os.path.join(output_path + "image-coordinate-2D/bright_01/", 'imgs_mask_test_%04d.jpg' % (index)), img)
+        # cv2.imwrite(os.path.join(output_path + "image-coordinate-2D/bright_01/", 'imgs_mask_test_%04d.jpg' % (index)), img)
+        cv2.imwrite(os.path.join(output_path + "image-coordinate-2D/bright_01/", '%s' % (image_name)), img)
         for xi in xrange(0, w):
             for xj in xrange(0, h):
                 ##set the pixel value increase to 1020%
@@ -34,5 +37,6 @@ if __name__ == '__main__':
                 img[xj, xi, 1] = int(img[xj, xi, 1] * 1000.2)
                 img[xj, xi, 2] = int(img[xj, xi, 2] * 1000.2)
         # cv2.imshow('img',img)
-        cv2.imwrite(os.path.join(output_path + "image-coordinate-2D/bright_02/", 'imgs_mask_test_%04d.jpg' % (index)), img)
+        # cv2.imwrite(os.path.join(output_path + "image-coordinate-2D/bright_02/", 'imgs_mask_test_%04d.jpg' % (index)), img)
+        cv2.imwrite(os.path.join(output_path + "image-coordinate-2D/bright_02/", '%s' % (image_name)), img)
         index += 1
